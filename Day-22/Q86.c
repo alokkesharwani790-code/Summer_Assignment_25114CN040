b920d21d9cc689d179cfd1c4d07a8f5bdd3ba360
alokkesharwani790-code <alokkesharwani790@gmail.com>
2026-06-18 11:28:33 +0530

    Day-22 uploaded

---
+++ Day-22/Q86.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i=0,words=1;
+    printf("Enter a sentence : ");
+    fgets(str,sizeof(str), stdin);
+    while(str[i]!='\0')
+    {
+        if(str[i]==' ')
+            words++;
+        i++;
+    }
+    printf("Number of Words = %d",words);
+    return 0;
+}
