823cf2d51dfe5e6e2952b25430f08baeb3323a1c
alokkesharwani790-code <alokkesharwani790@gmail.com>
2026-06-21 13:08:12 +0530

    Day-25 Uploaded

---
+++ Day-25/Q98.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    char str1[100],str2[100];
+    int i,j;
+    printf("Enter First string : ");
+    scanf("%s",str1);
+    printf("Enter Second string : ");
+    scanf("%s",str2);
+    printf("Common Characters : ");
+    for(i=0;str1[i]!='\0';i++)
+    {
+        for(j=0;str2[j]!='\0';j++)
+        {
+            if(str1[i]==str2[j])
+            {
+                printf("%c ",str1[i]);
+                break;
+            }
+        }
+    }
+    return 0;
+}
